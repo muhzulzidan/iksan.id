16d87fb0d44d91744317fb548238d94eb44a5c20
muhzulzidan <mzulzidan@gmail.com>
2024-05-08 15:52:56 +0800

    Remove unused imports and update types in columns.tsx, types.d.ts, header.tsx, and CategoryDropdownMenu.tsx

--- app/api/record-download/route.ts
+++ app/api/record-download/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
-
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from "next/navigation";
 export async function GET(req: NextRequest,) {
     const downloads = await prisma.downloadIksanId.findMany();
 
@@ -24,18 +25,27 @@ export async function POST(request: NextRequest) {
     const { userId, fileName } = await request.json();
 
     // const { userId, fileName } = JSON.parse(res);
+    // Check if the user is logged in
+    const user = await currentUser();
 
+
+    if (!user) {
+        return redirect('/sign-in');
+    } 
 
     if (!userId || !fileName) {
         return NextResponse.json({ error: 'Missing parameters' }, { status: 400 });
     }
 
     try {
+        
         const downloadRecord = await prisma.downloadIksanId.create({
             data: {
                 userId,
                 fileName: String(fileName),
                 downloadDate: new Date(),
+                fullname: user.firstName + ' ' + user.lastName,
+                email: user.emailAddresses[0].emailAddress,
             },
         });
 
