156fb4adffa6195abefb093b6161ae3c75de9589
muhzulzidan <mzulzidan@gmail.com>
2024-06-09 20:38:43 +0800

    refactor: Update UI styles for alert dialog, header, and sheet components

--- components/header.tsx
+++ components/header.tsx
@@ -131,6 +131,7 @@ export default function Header() {
       { href: "/kontak/", text: "Contact" }
     ],
     "Product": [
+      { href: "/kelas", text: "Kelas" },
       { href: "/template", text: "Template" },
       { href: "/video-course", text: "Ecourse" },
       { href: "/ebook", text: "Ebook" },
