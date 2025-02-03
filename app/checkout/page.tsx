f7d2f9a699066716c6b08dc235e158dd1c71742e
muhzulzidan <mzulzidan@gmail.com>
2025-02-03 23:14:59 +0800

    Refactor checkout page to redirect unauthenticated users to sign-in

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -2,12 +2,20 @@
 
 import Checkout from './checkoutClient';
 import { getUser } from '@/lib/getUser';
+import { redirect } from 'next/navigation'
 
 async function checkoutPage() {
+    // const router = useRouter();
+
     try {
         const user = await getUser();
+        if (!user) {
+            redirect('/sign-in'); // Redirect to sign-in page if user is not authenticated
+            return null;
+        }
         return <Checkout userData={user} />;
     } catch (error) {
+        redirect('/sign-in'); 
         console.error('Error in checkoutPage:', error);
         // Handle the error appropriately, e.g., show an error message or redirect
         return <div>Error loading checkout page. Please try again later.</div>;
