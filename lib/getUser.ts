feb816ac891903a29895669bfac1828c98878608
muhzulzidan <mzulzidan@gmail.com>
2025-02-12 23:47:23 +0800

    Add logging to getUser function for improved debugging of user retrieval and customer creation

--- lib/getUser.ts
+++ lib/getUser.ts
@@ -36,9 +36,6 @@
 // }
 
 // lib/getUser.ts
-
-// lib/getUser.ts
-
 import { currentUser } from "@clerk/nextjs/server";
 import { db } from '@/drizzle/db'; // Import your Drizzle ORM database instance
 import { eq } from 'drizzle-orm'; // Import the eq function from Drizzle ORM
@@ -47,13 +44,18 @@ import { customerIksanId } from '@/drizzle/schema'; // Import your Drizzle ORM s
 export async function getUser() {
     try {
         const user = await currentUser();
+        console.log('get-user Current user:', user);
+
         if (!user) {
             throw new Error('No user found');
         }
 
         if (user.emailAddresses && user.emailAddresses[0]) {
             const email = user.emailAddresses[0].emailAddress;
+            console.log('get-user  User email:', email);
+
             let customer = await db.select().from(customerIksanId).where(eq(customerIksanId.email, email)).execute();
+            console.log('get-user Customer found:', customer);
 
             if (!customer.length) {
                 customer = await db.insert(customerIksanId).values({
@@ -61,6 +63,7 @@ export async function getUser() {
                     name: `${user.firstName} ${user.lastName}`,
                     phoneNumber: '', // default value
                 }).returning().execute();
+                console.log('get-user New customer created:', customer);
             }
 
             return customer[0];
